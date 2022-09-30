954876c73866dc22d792868f19a731c733ef2e17
Chandradev <chandradev819@hotmail.com>
2022-09-30 11:58:01 +0530

    Popup code has been added.

---
+++ RadzenTest/Client/Pages/RadzenControls/ModelPopup.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RadzenTest.Client.Pages.RadzenControls
+{
+    public partial class ModelPopup : ComponentBase
+    {
+
+    }
+}
---
+++ RadzenTest/Client/Pages/RadzenControls/PopupCodeBehind.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+using Radzen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RadzenTest.Client.Pages.RadzenControls
+{
+    public partial class PopupCodeBehind : ComponentBase
+    {
+
+    }
+
+}
